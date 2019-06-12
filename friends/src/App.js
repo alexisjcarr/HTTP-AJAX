c72e464493d0baa4a9fa81d816623c88de969dd4
alexisjcarr <acarr59@gmail.com>
2019-06-12 15:53:58 -0500

    App renders everything. Now need torefactor using react router bc I was lazy earlier. Ugh

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
+import FriendsList from "./components/FriendsList/FriendsList";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: [],
+      friends: []
     };
   }
 
@@ -14,16 +16,20 @@ class App extends Component {
     axios
       .get("http://localhost:5000/friends")
       .then(res => {
-        console.log('response: ', res);
+        console.log("response: ", res);
         this.setState({
-          friends: res.data,
-        })
+          friends: res.data
+        });
       })
       .catch(err => console.log(err));
   }
 
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>
+        <FriendsList list={this.state.friends} />
+      </div>
+    );
   }
 }
 
