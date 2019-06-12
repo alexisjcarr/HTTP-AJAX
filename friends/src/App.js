75f33bbbd5cd3e7190b428d490955019486249c3
alexisjcarr <acarr59@gmail.com>
2019-06-12 15:21:58 -0500

    Initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        console.log('response: ', res);
+        this.setState({
+          friends: res.data,
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+export default App;
